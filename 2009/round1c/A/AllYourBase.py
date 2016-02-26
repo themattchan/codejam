390476ae54746b590117e1ac89b1db4bb1914061
Matthew Chan <matt@themattchan.com>
2016-02-25 18:12:41 -0800

    oh shit we can't assume anything about the symbols themselves...

--- 2009/round1c/A/AllYourBase.py
+++ 2009/round1c/A/AllYourBase.py
@@ -2,18 +2,18 @@ import sys
 from itertools import *
 
 def minBase(s):
-    def tryConvert(s,i):
-        try:
-            return int(s,i)
-        except:
-            return None
-    tries = [tryConvert(s,i) for i in range(2,37)]
-    return min(filter(lambda x: x != None, tries))
+    alpha = ".0123456789abcdefghijklmnopqrstuvwxyz"
+
+    b = alpha.index(max(s))
+    m = 0
+    for i,c in enumerate(s[::-1], start=0):
+        m += (int(c,b) * (b**i))
+    return m
 
 if __name__ == '__main__':
     sys.stdin.readline()        # discard # testcases
     with open('A_out', 'w') as out:
         i = 1
         for test in sys.stdin:
-            out.write("Case #%d: %d\n" % (i, minBase(test)))
+            out.write("Case #%d: %d\n" % (i, minBase(test.strip())))
             i += 1
