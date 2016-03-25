5f200c9748097a15a7b49a674fd06824221dae83
Matthew Chan <matt@themattchan.com>
2016-03-24 21:29:28 -0700

    fixed

--- 2010/main/1B/A/file-fix-it.rkt
+++ 2010/main/1B/A/file-fix-it.rkt
@@ -14,9 +14,11 @@
     (if (null? path) `(,ds ,count)
         (match-let*
             ((subdir 
-              (hash-ref ds (car path) (make-hash)))
+              (hash-ref ds (car path) null))
              (`(,subdir1 ,count1) 
-              (go subdir (cdr path) (if (hash-empty? subdir) (add1 count) count))))
+              (go (if (null? subdir) (make-hash) subdir) 
+                  (cdr path) 
+                  (if (null? subdir) (add1 count) count))))
           (hash-set! ds (car path) subdir1)
           `(,ds ,count1))))                               
   (go ds path 0))
@@ -37,7 +39,7 @@
                (for/list ([_ (range 0 N-dataset)]) (parse-path (read-line)))))
        
        (cc
-        (foldl (λ (path acc)                  
+        (foldl (λ (path acc)
                  (match-let* ((`(,filesystem ,count) acc)
                               (`(,filesystem1 ,count1) (get-path-or-mkdir filesystem path)))
                    `(,filesystem1 ,(+ count count1))))
@@ -46,13 +48,14 @@
     
     (cadr cc)
     ))
+;(run-case)
 
 (define (main)
-  (let ((out (open-output-file "A-small-practice.out" #:mode 'text #:exists 'replace)))
-    (current-output-port out)
+  (let ((out (open-output-file "A-large-practice.out" #:mode 'text #:exists 'replace)))
+    (parameterize ([current-output-port out])
     (for ((i (range 0 (string->number (read-line)))))
       (displayln (~a "Case #" (add1 i) ": " (run-case))))
-    (close-output-port out)))
+    (close-output-port out))))
                      
-(with-input-from-file "A-small-practice.in" main #:mode 'text)
+(with-input-from-file "A-large-practice.in" main #:mode 'text)
 
