5440c7248abbe23e3fa8a740c065c8dadcf95aeb
Matthew Chan <matt@awakenetworks.com>
2017-10-03 20:09:15 -0700

    pancakes

---
+++ 2016/quals/B/flippancake.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+import Data.Bifunctor
+import Control.Monad
+-- assume left is top
+flipcake :: String -> Int
+flipcake s
+  | all (== '+') s = 0
+  | otherwise =
+    let (ms, (ps, rest)) = second (span (== '+')) . span (=='-') $ s
+    in case rest of
+      [] -> mflip ms -- if rest is nothing, then just flip top '-'s and we are done
+      _  -> mflip ms + mflip ps + flipcake rest
+  where
+    mflip [] = 0
+    mflip  _ = 1
+
+{-
+   - + - -
+=> + + - -
+=> - - + +
+=> + + + +
+-}
+
+
+main :: IO ()
+main = do
+  n :: Int <- readLn
+  forM_ [1..n] $ \i -> do
+    s <- getLine
+    let r = flipcake s
+    putStrLn $ "Case #" ++ show i ++ ": " ++ show r
